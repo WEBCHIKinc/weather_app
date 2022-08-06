95991d775acb2bdbfca97c9b2b7698c912ac5f9c
WEBCHIKinc <kozerenkod@gmail.com>
2022-08-06 23:45:13 +0300

    Кнопка с переключением видеофона

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css'
-import React from "react";
+import React, { useState } from "react";
 import rain from './videos/rain.mp4'
+import clouds from './videos/clouds.mp4'
 
 function App() {
+  const [video, setVideo] = useState(clouds);
+  const changeBackgroundVideo = (video) => {
+    switch (video) {
+      case clouds:
+        setVideo(rain)
+        break;
+      case rain:
+        setVideo(clouds)
+        break;
+      default:
+        console.log(`video is not available`);
+    }
+  }
+
   return (
     <div className="App">
-      <video src={rain} autoPlay loop muted id="rainVideo"/>
-      <button>knopka</button>
+      <div className='default-background'></div>
+      <div className='rainVideo'><video src={video} autoPlay loop muted /></div>
+      <button onClick={() => changeBackgroundVideo(video)}>knopka</button>
     </div>
   );
 }
