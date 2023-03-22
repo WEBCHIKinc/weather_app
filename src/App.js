d238e9c96670053bb0617b1ecccc27b9c572b7c4
WEB_inc <kozerenkod@gmail.com>
2023-03-22 21:22:50 +0200

    different mini components removed. MiniBox used instead

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import DataBox from "./components/DataBox";
 import InputBox from "./components/InputBox";
 import MyErrorBox from "./components/UI/AlertBox/MyErrorBox";
 import MyDefaultBack from "./components/UI/DefaultBackground/MyDefaultBack";
-import LondonBox from "./components/LondonBox";
-import NewYorkBox from "./components/NewYorkBox";
-import KyivBox from "./components/KyivBox";
-import IpCityBox from "./components/IpCityBox";
+import MiniBox from "./components/MiniBox";
 import { useActions } from "./hooks/useActions";
-import TorontoBox from "./components/TorontoBox";
 import MyButton from "./components/UI/Button/MyButton";
 
 const DARK_THEME = "dark";
 const LIGHT_THEME = "light";
+const IP_TYPE = "ip";
 
 function App() {
   const { weatherData, isError } = useSelector((state) => state.weather);
@@ -23,6 +20,13 @@ function App() {
   const [currentTheme, setTheme] = useState(
     localStorage.getItem("Theme") || DARK_THEME
   );
+  const {
+    londonWeatherData,
+    kyivWeatherData,
+    newYorkWeatherData,
+    torontoWeatherData,
+    ipWeatherData,
+  } = useSelector((state) => state.weather);
 
   useEffect(() => {
     getMainCitiesWeather();
@@ -48,11 +52,15 @@ function App() {
       >
         <MyErrorBox />
       </CSSTransition>
-      <LondonBox />
-      <NewYorkBox />
-      <KyivBox />
-      <TorontoBox />
-      <IpCityBox />
+      <MiniBox cityName={"london"} weatherData={londonWeatherData} />
+      <MiniBox cityName={"kyiv"} weatherData={kyivWeatherData} />
+      <MiniBox cityName={"new york"} weatherData={newYorkWeatherData} />
+      <MiniBox cityName={"toronto"} weatherData={torontoWeatherData} />
+      <MiniBox
+        cityName={ipWeatherData.name}
+        weatherData={ipWeatherData}
+        type={IP_TYPE}
+      />
       {weatherData ? <DataBox /> : <InputBox />}
     </div>
   );
